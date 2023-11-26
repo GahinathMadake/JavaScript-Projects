20790deda8aee33fc2ee8f25627b8ae0ed408c9b
gahinathmadake <gahinathmadake@gmail.com>
2023-11-27 02:17:03 +0530

    sixth Project

---
+++ Calculator/script.js
@@ -0,0 +1,10 @@
+let display = document.querySelector('#box input');
+
+let calculate = document.querySelector('.equal');
+
+calculate.addEventListener('click', ()=>{
+    display.value = eval(display.value);
+
+
+    console.log(Object.eval("5*******5"));
+});
